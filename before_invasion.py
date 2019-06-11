47ca65483bf1a5697b52caf91adada771e356151
XingyuXiong <Peter__xiong@163.com>
2019-06-11 11:30:28 +0800

    使用sympy的未受外来物种入侵多维方程组

--- before_invasion.py
+++ before_invasion.py
@@ -2,21 +2,36 @@ import numpy as np
 import sympy as sy
 
 
+N=4
+
+
 def init():
-    n=10
-    p=t=c=m=np.zeros(n)
-    return p,t,c,m,n
+    n=N
+    t=m=np.zeros(n)
+    c=np.ones(n)
+    return t,c,m,n
 
 
 def before_invasion(args):
     '''
     以上为未受外来物种入侵的多物种竞争共存的集合种群模型
     所有参数均为数组np.array，pi表示物种i占据生境斑块的比例; ci表示物种i的扩散率; mi表示物种i的灭绝率; n表示物种种数。
+    直接解各个物种比例稳定值方程组，此处p为符号数组
     '''
-    p,t,c,m,n=args
-    derive=sy.diff(p,t,1)
-    block=np.array([1-np.sum(p[:i]) for i in range(n)])#剩余的生境斑块
-    spread=p*t*block#物种扩散部分
-    instinct=m*p#灭绝
-    unknown=np.array([np.sum(c*p[:i])*p for i in range(n)])#这部分意思没看懂
-    return -derive+spread-instinct-unknown
+    t,c,m,n=args
+    pi=sy.symbols('p1:5')
+    #将方程组分离为增广矩阵形式，即常数项单独分离开来
+    ##求pj从0到i-1的和，是以i为下标的数组，以下矩阵会用到
+    #注意下面下标是错位的，从0开始
+    para=[[0 for i in range(n)] for i in range(n)]#常数项矩阵
+    for i in range(n):
+        for j in range(n):
+            if j<i:
+                para[i][j]=1+int(c[j]/c[i])#int后期略去
+            elif i==j:
+                para[i][j]=1
+    para=sy.Matrix(para)
+    para2=[]#系数项矩阵
+    for i in range(n):
+        para2.append(m[i]/c[i]-1)
+    print(para,para2)
---
+++ test.py
@@ -0,0 +1,5 @@
+from sympy import *
+
+
+n=10
+print(symbols('x1:n+1'))
